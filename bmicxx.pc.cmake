ff541309c5412f286712318bf8f29f1435682234
Mark Piper <mark.piper@colorado.edu>
2023-10-24 15:00:02 -0600

    Improve information in pkg-config file

--- bmicxx.pc.cmake
+++ bmicxx.pc.cmake
@@ -1,5 +1,6 @@
-Name: Bmi
-Description: The Basic Model Interface
+Name: bmi-cxx
+Description: The Basic Model Interface for C++
+URL: https://bmi.readthedocs.io
 Version: ${BMI_VERSION}
-Libs: -L${CMAKE_INSTALL_LIBDIR} -lbmicxx
+Libs: -L${CMAKE_INSTALL_LIBDIR} -l${CMAKE_PROJECT_NAME}
 Cflags: -I${CMAKE_INSTALL_INCLUDEDIR}
